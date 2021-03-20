9f7ceadbfd5c17f6d3c98c3a932569c6a6688ba3
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-03-20 18:27:51 -0500

    added mongoose, and api posts profile and users

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://aleksandarac3:5JpWgpP8s3C72Cr@cluster0.tyga2.mongodb.net/test",
+};
--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("dsadsa"));
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected!"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Mongo DB"));
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+app.use("/api/profile", profile);
 
 const port = process.env.PORT || 5000;
 
