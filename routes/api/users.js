ee2b43bc442efaf6aebc5922dcf888355de20b58
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-03-21 22:11:08 -0500

    validation register and login

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load input validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 //Load User Model
 const User = require("../../models/User");
 
@@ -18,9 +22,17 @@ router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 //@desc   Register user
 //@access Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then((user) => {
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json({ email: errors });
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //size
@@ -52,6 +64,13 @@ router.post("/register", (req, res) => {
 //@access Public
 
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  //check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
@@ -60,7 +79,8 @@ router.post("/login", (req, res) => {
     .then((user) => {
       //check the user
       if (!user) {
-        return res.status(404).json({ email: "User not found" });
+        errors.email = "User not found";
+        return res.status(404).json(errors);
       }
       //check psw
       bcrypt
@@ -87,7 +107,8 @@ router.post("/login", (req, res) => {
               }
             );
           } else {
-            return res.status(400).json({ password: "Error" });
+            errors.password = "Password incorrect";
+            return res.status(400).json(errors);
           }
         })
         .catch();
