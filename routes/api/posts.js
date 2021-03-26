3a25fad9673f118f2a1d6a1d68d4fbb20b5f83cb
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-03-25 20:11:07 -0500

    like and unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -81,4 +81,69 @@ router.delete(
   }
 );
 
+//@route  POST api/posts/like/:id
+//@desc   like post
+//@access Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ usee: req.params.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyLiked: "User already liked this post" });
+          }
+          //add user id to likes array
+          post.likes.unshift({ user: req.user.id });
+          post.save().then((post) => res.json(post));
+        })
+        .catch((err) =>
+          res.status(404).json({ postNotFound: "Post not found" })
+        );
+    });
+  }
+);
+
+//@route  POST api/posts/unlike/:id
+//@desc   like post
+//@access Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ usee: req.params.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length == 0
+          ) {
+            return res
+              .status(400)
+              .json({ notLiked: "You have not yet liked this post" });
+          }
+          //get remove index
+          const removeIndex = post.likes
+            .map((item) => item.user.toString())
+            .indexOf(req.user.id);
+
+          //splice out of array
+          post.likes.splice(removeIndex, 1);
+
+          //save
+          post.save().then((post) => res.json(post));
+        })
+        .catch((err) =>
+          res.status(404).json({ postNotFound: "Post not found" })
+        );
+    });
+  }
+);
+
 module.exports = router;
