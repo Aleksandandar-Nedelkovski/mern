459694f9cf51ae9654fabc8ba59981978a1501aa
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-03-25 20:51:13 -0500

    add and remove comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -146,4 +146,68 @@ router.post(
   }
 );
 
+//@route  POST api/posts/comment/:id
+//@desc   add comment to post
+//@access Private
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+    //check validation
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+    Post.findById(req.params.id)
+      .then((post) => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id,
+        };
+        //add to comment array
+
+        post.comments.unshift(newComment);
+
+        post.save().then((post) => res.json(post));
+      })
+      .catch((err) => res.status(404).json({ postNotFound: "Post not found" }));
+  }
+);
+
+//@route  DELETE api/posts/comment/:id/:comment_id
+//@desc   delete comment from post
+//@access Private
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then((post) => {
+        //check if comment exists
+        if (
+          post.comments.filter(
+            (comment) => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentDoesNotExists: "Comment does not exists" });
+        }
+        //remove index
+        const removeIndex = post.comments
+          .map((item) => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        //splice comment out of array
+        post.comment.splice(removeIndex, 1);
+        post.save().then((post) => res.json(post));
+      })
+      .catch((err) =>
+        res.status(404).json({ commentNotFound: "Comment not found" })
+      );
+  }
+);
+
 module.exports = router;
