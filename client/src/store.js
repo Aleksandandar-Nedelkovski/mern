861a3844f4bd72fd5691b8cd11de6e5a3ca7cfeb
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-03-28 04:09:58 -0500

    reorganize entire project

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import rootReducer from "../src/reducers";
 
 const initialState = {};
 
