1e433c5770297c9e3b440df75a01aecfbc06d655
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-03-28 04:38:12 -0500

    changes in login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -32,7 +32,6 @@ class Login extends Component {
         <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
           <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
             <h1 className="mb-4 text-3xl text-center">Log In</h1>
-            <p className="mb-4 text-center">Create your BUILT account</p>
             <form onSubmit={this.onSubmit}>
               <input
                 type="text"
