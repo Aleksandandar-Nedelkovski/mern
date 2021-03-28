dcea614e6d23d4b7fb877b85509ffe62eedbd99b
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-03-28 03:48:10 -0500

    fix

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import { withRouter } from "react-router-dom";
-
-import classnames from "classnames";
-import { connect } from "react-redux";
-import { registerUser } from "../../actions/authActions";
+import axios from "axios";
 
 class Register extends Component {
   constructor() {
