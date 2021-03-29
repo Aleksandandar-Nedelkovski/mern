5dc81a59506d5368c71c71e087c0e0160026cad1
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-03-29 04:06:03 -0500

    login user succesfully, JSON web token, and localstorage

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import setAuthToken from "../utils/setAuthToken";
+import jwt_decode from "jwt-decode";
+
+import { GET_ERRORS, SET_CURRENT_USER } from "./types";
 
 //Register User
 export const registerUser = (userData, history) => (dispatch) => {
@@ -12,4 +15,36 @@ export const registerUser = (userData, history) => (dispatch) => {
         payload: err.response.data,
       })
     );
+};
+
+//login user
+export const loginUser = (userData) => (dispatch) => {
+  axios
+    .post("/api/users/login", userData)
+    .then((res) => {
+      //save to localstorage
+      const { token } = res.data;
+      //set token to localstorage
+      localStorage.setItem("jwtToken", token);
+      //set token to auth header
+      setAuthToken(token);
+      //decode token to get user data
+      const decoded = jwt_decode(token);
+      //set current user
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+//set loged in user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded,
+  };
 };
