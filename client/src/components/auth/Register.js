3218a75c0cd1346abaea8e0c402a3ea9dd1407ac
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-04-01 21:09:34 -0500

    added spinner

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { withRouter } from "react-router";
-import TextFieldGroup from "../common/textFieldGroup";
+import TextFieldGroup from "../common/TextFieldGroup";
 
 import { registerUser } from "../../actions/authActions";
 
