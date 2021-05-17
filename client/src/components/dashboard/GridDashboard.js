679d83b1206f8793e941a64fd36e624cf4287a3f
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-05-17 13:43:50 -0500

    first change since deploy

--- client/src/components/dashboard/GridDashboard.js
+++ client/src/components/dashboard/GridDashboard.js
@@ -6,75 +6,102 @@ export default function GridDashboard() {
     <section className="dashboard">
       <div className="container mx-auto">
         <div className="justify-center flex flex-wrap">
-          <div className="">
-            <div className="flex flex-wrap">
+          <div className="flex flex-wrap">
+            <Link to="/home-challenges">
               <div className="flex flex-wrap">
-                <Link to="/home-challenges">
-                  <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
-                    <img
-                      alt="..."
-                      className="align-middle border-none max-w-full h-auto rounded-lg"
-                      src={require("../../img/Slider/Hero-Image-2.jpg").default}
-                    />
-                  </div>
-                </Link>
-                <h5 className="text-xl font-semibold pb-4 text-center">
-                  Home Challenges
-                </h5>
-              </div>
-              <div className="w-full lg:w-4/12 px-4">
-                <Link to="/events">
+                <div className="w-full lg:w-4/12 px-4">
                   <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
                     <img
                       alt="..."
                       className="align-middle border-none max-w-full h-auto rounded-lg"
                       src={
-                        require("../../img/Characters/Characters-10.jpg")
+                        require("../../img/home-challenges/Home-Challenge-1.png")
                           .default
                       }
                     />
                   </div>
-                </Link>
-                <h5 className="text-xl font-semibold pb-4 text-center">
-                  Family Fun Day
-                </h5>
-              </div>
+                </div>
 
-              <div className="w-full lg:w-4/12 px-4">
-                <Link to="/schedule">
+                <div className="w-full lg:w-4/12 px-4">
                   <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
                     <img
                       alt="..."
                       className="align-middle border-none max-w-full h-auto rounded-lg"
                       src={
-                        require("../../img/Characters/Characters-09.jpg")
+                        require("../../img/home-challenges/Home-Challenge-2.png")
                           .default
                       }
                     />
                   </div>
-                </Link>
-                <h5 className="text-xl font-semibold pb-4 text-center">
-                  Schedule
-                </h5>
-              </div>
+                  <h5 className="text-xl font-semibold pb-4 text-center text-black">
+                    Home Challenges
+                  </h5>
+                </div>
 
-              <div className="w-full lg:w-4/12 px-4">
-                <Link to="/tickets">
+                <div className="w-full lg:w-4/12 px-4">
                   <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
                     <img
                       alt="..."
                       className="align-middle border-none max-w-full h-auto rounded-lg"
                       src={
-                        require("../../img/Characters/Characters-07.jpg")
+                        require("../../img/home-challenges/Home-Challenge-3.png")
                           .default
                       }
                     />
                   </div>
-                </Link>
-                <h5 className="text-xl font-semibold pb-4 text-center">
-                  Tickets
-                </h5>
+                </div>
               </div>
+            </Link>
+
+            <div className="w-full lg:w-4/12 px-4">
+              <Link to="/events">
+                <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                  <img
+                    alt="..."
+                    className="align-middle border-none max-w-full h-auto rounded-lg"
+                    src={
+                      require("../../img/Characters/Characters-10.jpg").default
+                    }
+                  />
+                </div>
+              </Link>
+              <h5 className="text-xl font-semibold pb-4 text-center">
+                Family Fun Day
+              </h5>
+            </div>
+
+            <div className="w-full lg:w-4/12 px-4">
+              <Link to="/schedule">
+                <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                  <img
+                    alt="..."
+                    className="align-middle border-none max-w-full h-auto rounded-lg"
+                    src={
+                      require("../../img/Characters/Characters-09.jpg").default
+                    }
+                  />
+                </div>
+              </Link>
+              <h5 className="text-xl font-semibold pb-4 text-center">
+                Schedule
+              </h5>
+            </div>
+
+            <div className="w-full lg:w-4/12 px-4">
+              <Link to="/tickets">
+                <div className="hover:-mt-4 relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg ease-linear transition-all duration-150">
+                  <img
+                    alt="..."
+                    className="align-middle border-none max-w-full h-auto rounded-lg"
+                    src={
+                      require("../../img/Characters/Characters-07.jpg").default
+                    }
+                  />
+                </div>
+              </Link>
+              <h5 className="text-xl font-semibold pb-4 text-center">
+                Tickets
+              </h5>
             </div>
           </div>
         </div>
