18f04373f43270fe763a1c14161514c7a4a5adb2
Aleksandar Nedelkovski <aleksn.dev@gmail.com>
2021-05-20 22:42:37 -0500

    courses page

--- client/src/components/course/Course.js
+++ client/src/components/course/Course.js
@@ -16,6 +16,21 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Add from "@material-ui/icons/AddBox";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Edit from "@material-ui/icons/Edit";
+import PeopleIcon from "@material-ui/icons/Group";
+import CompletedIcon from "@material-ui/icons/VerifiedUser";
+import Enroll from "./../enrollment/Enroll";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ListItemText from "@material-ui/core/ListItemText";
 
 const useStyles = makeStyles((theme) => ({
   root: theme.mixins.gutters({
@@ -83,14 +98,15 @@ const useStyles = makeStyles((theme) => ({
 const Course = ({
   getCourse,
   deleteCourse,
-  auth,
+  auth: { user },
   course: {
     _id,
     name,
     description,
     loading,
     category,
-    user,
+    course,
+
     published,
     lessons,
   },
@@ -102,8 +118,14 @@ const Course = ({
 
   const classes = useStyles();
   const [open, setOpen] = useState(false);
-  const [course, setCourse] = useState({ instructor: {} });
-
+  const [stats, setStats] = useState({});
+  console.log("====================================");
+  console.log("Course", course);
+  console.log("====================================");
+  // const imageUrl = course._id
+  //   ? `/api/courses/photo/${course._id}?${new Date().getTime()}`
+  //   : "/api/courses/defaultphoto";
+  const imageUrl = require("../../img/logo.png").default;
   const clickPublish = () => {
     if (lessons.length > 0) {
       setOpen(true);
@@ -146,46 +168,178 @@ const Course = ({
   };
 
   const addLesson = (course) => {
-    setCourse(course);
+    // setCourse(course);
   };
 
   return loading || course === null ? (
     <Spinner />
   ) : (
-    <Fragment>
-      <Link to="/courses" className="btn">
-        Back To Courses
-      </Link>
-      <CourseItem course={course} showActions={false} />
-      <span className={classes.action}>
-        <NewLesson courseId={course._id} addLesson={addLesson} />
-      </span>
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <CardHeader
+          title={course.name}
+          subheader={
+            <div>
+              <Link to={"/user/" + user._id} className={classes.sub}>
+                By {course.name}
+              </Link>
+              <span className={classes.category}>{course.category}</span>
+            </div>
+          }
+          action={
+            <>
+              {user && user._id === course._id && (
+                <span className={classes.action}>
+                  <Link to={"/teach/course/edit/" + course._id}>
+                    <IconButton aria-label="Edit" color="secondary">
+                      <Edit />
+                    </IconButton>
+                  </Link>
+                  {!course.published ? (
+                    <>
+                      <Button
+                        color="secondary"
+                        variant="outlined"
+                        onClick={clickPublish}
+                      >
+                        {course.lessons.length == 0
+                          ? "Add atleast 1 lesson to publish"
+                          : "Publish"}
+                      </Button>
+                      {/* <DeleteCourse course={course} onRemove={removeCourse} /> */}
+                    </>
+                  ) : (
+                    <Button color="primary" variant="outlined">
+                      Published
+                    </Button>
+                  )}
+                </span>
+              )}
+              {course.published && (
+                <div>
+                  <span className={classes.statSpan}>
+                    <PeopleIcon /> {stats.totalEnrolled} enrolled{" "}
+                  </span>
+                  <span className={classes.statSpan}>
+                    <CompletedIcon /> {stats.totalCompleted} completed{" "}
+                  </span>
+                </div>
+              )}
+            </>
+          }
+        />
+        <div className={classes.flex}>
+          <CardMedia
+            className={classes.media}
+            image={imageUrl}
+            title={course.name}
+          />
+          <div className={classes.details}>
+            <Typography variant="body1" className={classes.subheading}>
+              {course.description}
+              <br />
+            </Typography>
 
-      {!published ? (
-        <>
-          {/* <Button color="secondary" variant="outlined" onClick={clickPublish}>
-            {lessons.length === 0
-              ? "Add atleast 1 lesson to publish"
-              : "Publish"}
+            {course.published && (
+              <div className={classes.enroll}>
+                <Enroll courseId={course._id} />
+              </div>
+            )}
+          </div>
+        </div>
+        <Divider />
+        <div>
+          <CardHeader
+            title={
+              <Typography variant="h6" className={classes.subheading}>
+                Lessons
+              </Typography>
+            }
+            subheader={
+              <Typography variant="body1" className={classes.subheading}>
+                {course.lessons && course.lessons.length} lessons
+              </Typography>
+            }
+            action={
+              user &&
+              user._id == course._id &&
+              !course.published && (
+                <span className={classes.action}>
+                  <NewLesson courseId={course._id} addLesson={addLesson} />
+                </span>
+              )
+            }
+          />
+          <List>
+            {course.lessons &&
+              course.lessons.map((lesson, index) => {
+                return (
+                  <span key={index}>
+                    <ListItem>
+                      <ListItemAvatar>
+                        <Avatar>{index + 1}</Avatar>
+                      </ListItemAvatar>
+                      <ListItemText primary={lesson.title} />
+                    </ListItem>
+                    <Divider variant="inset" component="li" />
+                  </span>
+                );
+              })}
+          </List>
+        </div>
+      </Card>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">Publish Course</DialogTitle>
+        <DialogContent>
+          <Typography variant="body1">
+            Publishing your course will make it live to students for enrollment.{" "}
+          </Typography>
+          <Typography variant="body1">
+            Make sure all lessons are added and ready for publishing.
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" variant="contained">
+            Cancel
+          </Button>
+          {/* <Button onClick={publish} color="secondary" variant="contained">
+            Publish
           </Button> */}
-          {!auth.loading && user === auth.user._id && (
-            <button
-              onClick={() => deleteCourse(_id)}
-              type="button"
-              className="btn btn-danger"
-            >
-              <i className="fas fa-times" />
-            </button>
-          )}
-        </>
-      ) : (
-        <Button color="primary" variant="outlined">
-          Published
-        </Button>
-      )}
-    </Fragment>
+          {/* <CourseItem course={course} showActions={false} />
+    <span className={classes.action}>
+      <NewLesson courseId={course._id} addLesson={addLesson} />
+    </span> */}
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
+// {!published ? (
+//   <>
+//     {/* <Button color="secondary" variant="outlined" onClick={clickPublish}>
+//       {course.lessons.length === 0
+//         ? "Add atleast 1 lesson to publish"
+//         : "Publish"}
+//     </Button> */}
+//     {!auth.loading && user === auth.user._id && (
+//       <button
+//         onClick={() => deleteCourse(_id)}
+//         type="button"
+//         className="btn btn-danger"
+//       >
+//         <i className="fas fa-times" />
+//       </button>
+//     )}
+//   </>
+// ) : (
+//   <Button color="primary" variant="outlined">
+//     Published
+//   </Button>
+// )}
 
 Course.propTypes = {
   getCourse: PropTypes.func.isRequired,
